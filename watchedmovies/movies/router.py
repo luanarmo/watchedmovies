e1c4f43706c5142725d3d2b4e3aeeae364e955d2
Luis Angel Arroyo Morales <36710715+luanarmo@users.noreply.github.com>
2024-08-20 15:58:28 -0600

    feat: Agrega router para view details

--- watchedmovies/movies/router.py
+++ watchedmovies/movies/router.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import WatchedMovieViewSet
+from .views import ViewDetailViewSet, WatchedMovieViewSet
 
 router = DefaultRouter()
 
 router.register(r"watched-movies", WatchedMovieViewSet, basename="watched-movies")
+router.register(r"view-details", ViewDetailViewSet, basename="view-details")
