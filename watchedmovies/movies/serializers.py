0afa786b05d7e85f0f8e99b0c97f7c9c87030f9e
Luis Angel Arroyo Morales <36710715+luanarmo@users.noreply.github.com>
2024-08-20 16:00:57 -0600

    feat: Agrega serializers para modelo viewDetails

--- watchedmovies/movies/serializers.py
+++ watchedmovies/movies/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from watchedmovies.movies.models import WatchedMovie
+from watchedmovies.movies.models import ViewDetails, WatchedMovie
 
 
 class WatchedMovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = WatchedMovie
+        fields = "__all__"
+
+
+class ViewDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ViewDetails
         fields = "__all__"
