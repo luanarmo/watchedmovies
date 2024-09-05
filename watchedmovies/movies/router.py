4d6ade36b5d28a8c5a8b4ad6225d0d9ed908a51a
Luis Angel Arroyo Morales <36710715+luanarmo@users.noreply.github.com>
2024-09-05 15:46:36 -0600

    feat: implementacion de la vista nueva para consumir la api

--- watchedmovies/movies/router.py
+++ watchedmovies/movies/router.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import ViewDetailViewSet, WatchedMovieViewSet
+from .views import TMDBViewSet, ViewDetailViewSet, WatchedMovieViewSet
 
 router = DefaultRouter()
 
 router.register(r"watched-movies", WatchedMovieViewSet, basename="watched-movies")
 router.register(r"view-details", ViewDetailViewSet, basename="view-details")
+router.register(r"tmdb", TMDBViewSet, basename="tmdb")
