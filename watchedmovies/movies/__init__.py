7c22b6a9c37c166580f1f231da34f9e45d278a87
Luis Angel Arroyo Morales <36710715+luanarmo@users.noreply.github.com>
2024-09-06 10:49:22 -0600

    feat: Se agregan el tamaño de las imagenes disponibles

---
+++ watchedmovies/movies/__init__.py
@@ -0,0 +1,5 @@
+class ImageSizes:
+    """Class that defines the available image sizes."""
+
+    BACKDROP_SIZES = ("w300", "w780", "w1280", "original")
+    POSTER_SIZES = ("w92", "w154", "w185", "w342", "w500", "w780", "original")
