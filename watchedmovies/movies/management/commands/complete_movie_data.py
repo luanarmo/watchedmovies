18baef785030812067f635b5cc416e6cc3587363
luanarmo <luisangel_arroyo@outlook.com>
2024-10-11 09:50:34 -0600

    feat: comando para completar la informacion de las peliuclas ya registradas

---
+++ watchedmovies/movies/management/commands/complete_movie_data.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from watchedmovies.movies.services import tmdb_api
+from watchedmovies.movies.models import WatchedMovie
+
+
+class Command(BaseCommand):
+    """
+    This command completes the data of the movies. It makes a request to the TMDB API to get the details of each movie.
+    """
+
+    help = "This command completes the data of the movies. It makes a request to the TMDB API to get the details of each movie."
+
+    def handle(self, *args, **kwargs):
+
+        watched_movies = WatchedMovie.objects.all()
+
+        for watched_movie in watched_movies:
+            movie_details = tmdb_api.get_movie_details(watched_movie.pk)
+            watched_movie.runtime = movie_details.get("runtime")
+            watched_movie.more_details = movie_details
+            watched_movie.full_clean()
+            watched_movie.save()
+
+        self.stdout.write(self.style.SUCCESS("All movies have been updated successfully!"))
