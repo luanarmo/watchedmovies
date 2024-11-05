57f2a998a7c3978a77958f0487f4d5eb6604c511
luanarmo <luisangel_arroyo@outlook.com>
2024-11-05 16:50:36 -0600

    feat: Cambios en utileria para enviar correos

--- watchedmovies/users/utils/sendmail.py
+++ watchedmovies/users/utils/sendmail.py
@@ -2,12 +2,14 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
+from config.settings.base import env
 
-def send_emails(*, name: str, email: str) -> None:
-    subject = "Welcome to Watched Movies!"
-    html_message = render_to_string("success_mail.html", {"name": name})
+
+def send_email(*, to: str, template: str = "", subject: str = "", context: dict = {}) -> None:
+    host_email = env("EMAIL_HOST_USER")
+    subject = subject or "WatchedMovies"
+    html_message = render_to_string(template, context or {})
     plain_message = strip_tags(html_message)
-    from_email = "watchedmovies@noreply.com"
-    to = email
+    from_email = host_email
 
     send_mail(subject, plain_message, from_email, [to], html_message=html_message)
