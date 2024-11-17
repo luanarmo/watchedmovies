eeed4fd167107b5435c3bb1527c7c5de931728e5
luanarmo <luisangel_arroyo@outlook.com>
2024-11-17 10:13:53 -0600

    fix: ordenar peliculas vistas por fecha

--- watchedmovies/movies/services.py
+++ watchedmovies/movies/services.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+from django.db.models import Min
+
 from watchedmovies.services import tmdb_api
 from watchedmovies.users.models import Profile
 
@@ -63,7 +65,10 @@ def destroy_view_detail(*, watched_movie: WatchedMovie, profile) -> None:
 def create_collage(*, profile: Profile) -> str:
     """Create a collage from a list of poster URLs."""
     watched_movies = (
-        WatchedMovie.objects.filter(view_details__profile=profile).distinct().values_list("poster_path", flat=True)
+        WatchedMovie.objects.filter(view_details__profile=profile)
+        .annotate(first_watched_date=Min("view_details__watched_date"))
+        .order_by("-first_watched_date")
+        .values_list("poster_path", flat=True)
     )
 
     return generate_collage(poster_urls=watched_movies)
