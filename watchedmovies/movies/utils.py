cffa580b5062e193d4f6063faa1657701bec778c
luanarmo <luisangel_arroyo@outlook.com>
2024-12-29 15:06:21 -0600

    feat: Agregar utils para poner texto a imagen

--- watchedmovies/movies/utils.py
+++ watchedmovies/movies/utils.py
@@ -1,10 +1,10 @@
 import math
 import tempfile
 from concurrent.futures import ThreadPoolExecutor
-from typing import BinaryIO
+from typing import BinaryIO, Protocol
 
 import requests
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 from config.settings.base import env
 
@@ -70,3 +70,118 @@ def calculate_dimensions(n: int) -> tuple:
     cols = math.ceil(math.sqrt(n))
     rows = math.ceil(n / cols)
     return cols, rows
+
+
+class TextLine:
+    """Clase que representa una línea de texto con sus propiedades."""
+
+    def __init__(
+        self,
+        text: str,
+        font: ImageFont,
+        color: tuple[int, int, int],
+    ):
+        self.text = text
+        self.font = font
+        self.color = color
+
+
+class RenderStrategy(Protocol):
+    """Interfaz para las estrategias de renderizado."""
+
+    def render(self, canvas: "Canvas", lines: list[TextLine]) -> int:
+        """Renderiza el texto en el canvas."""
+        pass
+
+
+class OneColumnRenderStrategy(RenderStrategy):
+    """Estrategia de renderizado en una columna."""
+
+    def render(self, canvas: "Canvas", lines: list[TextLine]) -> None:
+        for line in lines:
+            text_bbox = canvas.draw.textbbox((0, 0), line.text, font=line.font)
+            text_height = text_bbox[3] - text_bbox[1]
+            canvas.draw.text(
+                (canvas.x_pos, canvas.y_pos),
+                line.text,
+                font=line.font,
+                fill=line.color,
+            )
+            canvas.y_pos += text_height + canvas.y_spacing
+
+
+class TwoColumnRenderStrategy(RenderStrategy):
+    """Estrategia de renderizado en dos columnas."""
+
+    def render(self, canvas: "Canvas", lines: list[TextLine]) -> None:
+        odd_lines = lines[::2]
+        even_lines = lines[1::2]
+
+        for line1, line2 in zip(odd_lines, even_lines):
+            text_bbox1 = canvas.draw.textbbox(
+                (0, 0),
+                line1.text,
+                font=line1.font,
+            )
+            text_bbox2 = canvas.draw.textbbox(
+                (0, 0),
+                line2.text,
+                font=line2.font,
+            )
+            text_height1 = text_bbox1[3] - text_bbox1[1]
+            text_height2 = text_bbox2[3] - text_bbox2[1]
+            canvas.draw.text(
+                (canvas.x_pos, canvas.y_pos),
+                line1.text,
+                font=line1.font,
+                fill=line1.color,
+            )
+            canvas.draw.text(
+                (canvas.x_pos + 500, canvas.y_pos),
+                line2.text,
+                font=line2.font,
+                fill=line2.color,
+            )
+            canvas.y_pos += max(text_height1, text_height2) + canvas.y_spacing
+
+
+class Canvas:
+    """Clase que representa un canvas para renderizar texto."""
+
+    def __init__(
+        self,
+        bg_img_path: str,
+        x_pos: int = 0,
+        y_pos: int = 0,
+        y_spacing: int = 40,
+        strategy: RenderStrategy = OneColumnRenderStrategy(),
+    ):
+        self.bg_img_path = bg_img_path
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.y_spacing = y_spacing
+        self.strategy = strategy
+        self.image = Image.open(self.bg_img_path)
+        self.draw = ImageDraw.Draw(self.image)
+
+    def set_strategy(self, strategy: RenderStrategy):
+        self.strategy = strategy
+
+    def render_text(self, lines: list[TextLine]) -> None:
+        """Renderiza el texto en el canvas."""
+        self.strategy.render(self, lines)
+
+    def save(self) -> BinaryIO:
+        """Guarda y muestra la imagen renderizada."""
+        temp_path = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
+        self.image.save(temp_path.name, "PNG")
+        temp_path.seek(0)
+        return temp_path.read()
+
+
+def load_font(path: str, size: int) -> ImageFont:
+    """Carga una fuente TrueType desde un archivo."""
+    try:
+        return ImageFont.truetype(path, size)
+    except OSError:
+        return ImageFont.load_default()
