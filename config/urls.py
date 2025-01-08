64d0e7bd7792f6a4a8aafa735b4c1cfcebb0940b
luanarmo <luisangel_arroyo@outlook.com>
2025-01-07 19:57:22 -0600

    feat: Agregar nueva ruta

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import token_refresh, token_verify
 
-from watchedmovies.users.views import CustomTokenObtainPairView, VerifyEmailTokenView
+from watchedmovies.users.views import CustomTokenObtainPairView, VerifyEmailTokenView, VerifyResetPasswordTokenView
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
@@ -29,6 +29,7 @@ urlpatterns += [
     path("api/auth/refresh/", token_refresh),
     path("api/auth/verify/", token_verify),
     path("api/auth/verify_email/<uid>/<token>/", VerifyEmailTokenView.as_view(), name="verify_email"),
+    path("api/auth/reset_password/<uid>/<token>/", VerifyResetPasswordTokenView.as_view(), name="reset_password"),
 ]
 
 if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
