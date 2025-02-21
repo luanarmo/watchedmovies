3d8648bec740cabdcf9e4aa02794ccdf546a49dd
luanarmo <luisangel_arroyo@outlook.com>
2025-02-21 08:27:13 -0600

    test: Eliminar test para endpoint que ya no existe

--- watchedmovies/users/tests/test_drf_urls.py
+++ watchedmovies/users/tests/test_drf_urls.py
@@ -6,11 +6,6 @@ def test_user_register():
     assert resolve("/api/anonymous/").view_name == "users:anonymous-list"
 
 
-def test_user_change_password():
-    assert reverse("users:anonymous-change_password") == "/api/anonymous/change_password/"
-    assert resolve("/api/anonymous/change_password/").view_name == "users:anonymous-change_password"
-
-
 def test_user_retrieve():
     assert reverse("users:users-me") == "/api/users/me/"
     assert resolve("/api/users/me/").view_name == "users:users-me"
