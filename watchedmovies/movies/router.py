dfb7bf7bb6d8dcaaaa41e3975e4e1980487ad166
luanarmo <luisangel_arroyo@outlook.com>
2025-03-05 21:36:28 -0600

    feat: agrega router para vistas de plantowatch

--- watchedmovies/movies/router.py
+++ watchedmovies/movies/router.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import TMDBViewSet, ViewDetailViewSet, WatchedMovieViewSet
+from .views import PlanToWatchViewSet, TMDBViewSet, ViewDetailViewSet, WatchedMovieViewSet
 
 router = DefaultRouter()
 
 router.register(r"watched-movies", WatchedMovieViewSet, basename="watched-movies")
 router.register(r"view-details", ViewDetailViewSet, basename="view-details")
 router.register(r"tmdb", TMDBViewSet, basename="tmdb")
+router.register(r"plan-to-watch", PlanToWatchViewSet, basename="plan-to-watch")
